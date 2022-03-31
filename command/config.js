cbfbe26eb0c45cfe44b780629a9a33cd385a9df9
RiyoXdz <mikumiki683@gmail.com>
2022-03-31 10:08:09 +0800

    Minami-Md

---
+++ command/config.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE API ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+zeroyt7: 'https://zeroyt7-api.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY WEBSITE API ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zeroyt7-api.xyz': 'yourkey',
+}
+
+//━━━━━━━━━━━━━━━[ OTHER ]━━━━━━━━━━━━━━━━━//
+
+global.ownername = 'Minami-Bot'//Nama Bot Kaliam
+global.owner = ['6281223918622']//Nomor Owner Kalian (Bebas)
+global.packname = 'RiyoXdz'//Bebas Mau Ganti Apa Gak
+global.author = 'RiypXdz'//Jangan Di Ganti 
+global.prefa = ['','!','.','🐦','🐤','🗿']//Jangan Di Ganti
+global.sp = '⭔'
+global.mess = {
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+eror: 'Yah eror',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: 'Loading...',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
